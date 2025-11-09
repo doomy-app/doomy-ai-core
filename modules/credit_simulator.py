19bbe7d2e16756206dd8544c8cf15c365491c104
doomy-app <appdoomy@gmail.com>
2025-11-08 22:50:07 -0600

    Add credit_simulator module

---
+++ modules/credit_simulator.py
@@ -0,0 +1,7 @@
+def simulate_credit(income, credit_type):
+    # Placeholder function to simulate credit options
+    return {
+        "monthly_payment": income * 0.3,
+        "max_credit": income * 20,
+        "credit_type": credit_type
+    }
